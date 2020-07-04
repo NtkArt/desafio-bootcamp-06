cf87bd1549729f82a5777430757c7f46b39689bd
Arthur Carneiro <arthur.carneirontk@gmail.com>
2020-07-04 17:58:16 -0300

    Creating and listing

--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -1,16 +1,35 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+  CreateDateColumn,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
+
+@Entity('transactions')
 class Transaction {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
   title: string;
 
+  @Column()
   type: 'income' | 'outcome';
 
+  @Column('decimal')
   value: number;
 
+  @ManyToOne(() => Transaction)
+  @JoinColumn({ name: 'category_id' })
   category_id: string;
 
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,28 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository } from 'typeorm';
 
+// import AppError from '../errors/AppError';
+import TransactionRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  type: 'income' | 'outcome';
+  value: number;
+  category: number;
+}
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({ title, value, type }: Request): Promise<Transaction> {
+    const transactionsRepository = getCustomRepository(TransactionRepository);
+
+    const transaction = transactionsRepository.create({
+      title,
+      type,
+      value,
+    });
+
+    await transactionsRepository.save(transaction);
+
+    return transaction;
   }
 }
 
